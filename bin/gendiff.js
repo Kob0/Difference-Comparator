3dcf2fbc21b5dcbbd928445e29a3c84f80092d67
andrew m <kaboom2012@mail.ru>
2020-05-18 22:37:46 +0300

    fix linter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,6 @@ const program = commander;
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a difference.');
 
 program.parse(process.argv);
