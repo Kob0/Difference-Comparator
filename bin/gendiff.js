33b28647df1ba155b9e9993339ecda0028853dab
andrew m <kaboom2012@mail.ru>
2020-05-18 22:27:46 +0300

    first commit

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env/ node
+
+const commander = require('commander');
+
+const program = commander;
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+
+program.parse(process.argv);
