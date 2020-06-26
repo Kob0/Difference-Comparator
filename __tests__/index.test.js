8fd8a809eb319b4e35bb9fd8d9a52db8085f53c9
andrew m <kaboom2012@mail.ru>
2020-06-26 20:53:59 +0300

    add jest globals

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,6 +4,13 @@ import getDiff from '../src/index.js';
 let result;
 
 beforeEach(() => {
-  result = ['{'];
-  result.push
-})
+  result = [
+    '{',
+    '    host: hexlet.io',
+    '  + timeout: 20',
+    '  - timeout: 50',
+    '  - proxy: 123.234.53.22',
+    '  - follow: false',
+    '  + verbose: true',
+    '}'].join('\n');
+});
