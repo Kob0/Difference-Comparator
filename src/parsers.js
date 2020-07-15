8ded293d0571572b6b2bf0139055a3b45d082559
andrew m <kaboom2012@mail.ru>
2020-07-15 22:55:18 +0300

    fix errors

--- src/parsers.js
+++ src/parsers.js
@@ -2,8 +2,20 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
+const getCorrectPathToFile = (pathToFile) => {
+  const currentDirectory = process.cwd();
+  let correctPath;
+  if (!currentDirectory.includes(pathToFile)) {
+    correctPath = path.resolve(currentDirectory, pathToFile);
+  } else {
+    correctPath = pathToFile;
+  }
+
+  return correctPath;
+};
+
 const parseFile = (filePath) => {
-  const data = fs.readFileSync(filePath, 'utf-8');
+  const data = fs.readFileSync(getCorrectPathToFile(filePath), 'utf8');
   const format = path.extname(data).substring(1);
 
   let parse;
