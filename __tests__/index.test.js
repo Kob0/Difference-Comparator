87d6b5f7d89defa00108f9b419928a75a77fc0eb
andrew m <kaboom2012@mail.ru>
2020-07-24 21:10:56 +0300

    fix imports

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-undef */
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import path from 'path';
 import getDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+let result;
 
 beforeEach(() => {
   result = [
