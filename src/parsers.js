03c3d08e1cc16c2f4ebcee1dbbb0ec99f8262af9
andrew m <kaboom2012@mail.ru>
2021-02-21 22:34:53 +0300

    full refacror of "parse" function

--- src/parsers.js
+++ src/parsers.js
@@ -1,31 +1,12 @@
-import fs from 'fs';
-import ini from 'ini';
-import path from 'path';
 import yaml from 'js-yaml';
 
-const getFullPath = (filePath) => {
-  const currentDir = process.cwd();
-  const fullPath = path.resolve(currentDir, filePath);
-
-  return fullPath;
-};
-
-const parseFile = (filePath) => {
-  const pathToFile = getFullPath(filePath);
-  const data = fs.readFileSync(pathToFile, 'utf8');
-  const format = path.extname(pathToFile);
-
-  let parse;
-
-  if (format === '.json') {
-    parse = JSON.parse;
-  } else if (format === '.yml') {
-    parse = yaml.safeLoad;
-  } else if (format === '.ini') {
-    parse = ini.parse;
+export default (data, format) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+      return yaml.safeLoad(data);
+    default:
+      throw new Error(`Unkwond data format: ${format}`);
   }
-
-  return parse(data);
 };
-
-export default parseFile;
