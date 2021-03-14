dcf18cc940504418d9e1fb7e16c7df991dece87f
andrew m <kaboom2012@mail.ru>
2021-03-14 15:23:41 +0300

    change all structure of tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,41 +1,29 @@
 /* eslint-disable no-undef */
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import fs from 'fs';
 import getDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let result;
+const cases = [
+  ['json', 'stylish', 'exectedStylish.txt'],
+  ['yml', 'stylish', 'expectedStylish.txt'],
+];
 
-beforeEach(() => {
-  result = [
-    '{',
-    '    host: hexlet.io',
-    '  + timeout: 20',
-    '  - timeout: 50',
-    '  - proxy: 123.234.53.22',
-    '  - follow: false',
-    '  + verbose: true',
-    '}'].join('\n');
-});
-
-test('getDiff for json files', () => {
-  const jsonPath1 = getFixturePath('file1.json');
-  const jsonPath2 = getFixturePath('file2.json');
-  expect(getDiff(jsonPath1, jsonPath2)).toEqual(result);
-});
-
-test('getDiff for yaml files', () => {
-  const yamlPath1 = getFixturePath('file1.yml');
-  const yamlPath2 = getFixturePath('file2.yml');
-  expect(getDiff(yamlPath1, yamlPath2)).toEqual(result);
-});
-
-test('getDiff for ini files', () => {
-  const iniPath1 = getFixturePath('file1.ini');
-  const iniPath2 = getFixturePath('file2.ini');
-  expect(getDiff(iniPath1, iniPath2)).toEqual(result);
+describe('test genDiff, each case in order', () => {
+  test.each(cases)(
+    'files in type %p formatted in %p and expected to be as %p',
+    (type, format, expectedResult) => {
+      const file1 = getFixturePath(`1.${type}`);
+      const file2 = getFixturePath(`2.${type}`);
+      const diff = getDiff(file1, file2, format);
+      const result = readFile(expectedResult).trim();
+      expect(diff).toEqual(result);
+    },
+  );
 });
