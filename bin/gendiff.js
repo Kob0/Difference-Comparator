c145974ab8f77f416b5dacf82ae2b449cabccde8
andrew m <kaboom2012@mail.ru>
2021-03-19 02:03:16 +0300

    enable multi-formatters functionality

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,7 +11,7 @@ const genDiff = program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'choose output format: stylish | plain | json', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(getDiff(filepath1, filepath2));
+    console.log(getDiff(filepath1, filepath2, program.format));
   });
 
 program.parse(process.argv);
