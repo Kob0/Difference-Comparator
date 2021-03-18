71e154a6d965c48aff170d91fe7b8d20c1940334
andrew m <kaboom2012@mail.ru>
2021-03-18 05:26:51 +0300

    fix syntax error

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js';
+import format from './formatters/index.js';
 
 const makeDiff = (obj1, obj2) => {
-  const keys = _.union(_.keys(obj1), _.keys(obj2)).sort();
+  const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
   return keys.map((key) => {
     if (!_.has(obj1, key)) {
       return { name: key, status: 'added', value: obj2[key] };
@@ -12,11 +13,11 @@ const makeDiff = (obj1, obj2) => {
     if (!_.has(obj2, key)) {
       return { name: key, status: 'deleted', value: obj1[key] };
     }
-    if (obj1[key] === obj2[key]) {
-      return { name: key, status: 'unmodified', value: obj1[key] };
-    }
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
       return { name: key, status: 'unknown', children: makeDiff(obj1[key], obj2[key]) };
+    }
+    if (obj1[key] === obj2[key]) {
+      return { name: key, status: 'unmodified', value: obj1[key] };
     }
     return {
       name: key, status: 'modified', oldValue: obj1[key], newValue: obj2[key],
@@ -24,7 +25,7 @@ const makeDiff = (obj1, obj2) => {
   });
 };
 
-const getDiff = (filepath1, filepath2) => {
+export default (filepath1, filepath2, outputFormat) => {
   const currentDir = process.cwd();
   const fullPath1 = path.resolve(currentDir, filepath1);
   const fullPath2 = path.resolve(currentDir, filepath2);
@@ -33,8 +34,7 @@ const getDiff = (filepath1, filepath2) => {
   const getFormat = (filePath) => path.extname(filePath).slice(1);
   const obj1 = parse(data1, getFormat(fullPath1));
   const obj2 = parse(data2, getFormat(fullPath2));
+  const diff = makeDiff(obj1, obj2);
 
-  return makeDiff(obj1, obj2);
+  return format(diff, outputFormat);
 };
-
-console.log(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json'));
