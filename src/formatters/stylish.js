92a0e9eaaab0a93124af7650a520f8d35758c20e
andrew m <kaboom2012@mail.ru>
2021-03-18 05:16:13 +0300

    add few syntax changes

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -5,6 +5,7 @@ const getFormat = (value) => {
     return value;
   }
   const keys = _.keys(value);
+
   return keys.map((key) => {
     if (!_.isObject(value[key])) {
       return { name: key, status: 'unknown', children: value[key] };
@@ -22,26 +23,26 @@ export default (diff) => {
       const space = ' ';
       const defaultIndent = space.repeat(4);
       const defaultIndent2 = space.repeat(2);
-      const indent = defaultIndent + space.repeat(depth * 4);
+      const indent1 = defaultIndent + space.repeat(depth * 4);
       const indent2 = defaultIndent2 + space.repeat(depth * 4);
 
-      const chooseIndent = (v) => (_.isArray(v) ? `{\n${iter(v, depth + 1).join('\n')}\n}` : v);
+      const chooseIndent = (v) => (Array.isArray(v) ? `{\n${iter(v, depth + 1).join('\n')}\n${indent1}}` : v);
 
-      const formatValue = (val) => chooseIndent(getFormat(val));
+      const format = (val) => chooseIndent(getFormat(val));
 
       switch (status) {
         case 'added':
-          return `${indent2}+ ${name}: ${formatValue(value)}`;
+          return `${indent2}+ ${name}: ${format(value)}`;
         case 'deleted':
-          return `${indent2}- ${name}: ${formatValue(value)}`;
-        case 'unmodified':
-          return `${indent}${name}: ${formatValue(value)}`;
+          return `${indent2}- ${name}: ${format(value)}`;
         case 'unknown':
-          return `${indent}${name}: ${chooseIndent(children)}`;
+          return `${indent1}${name}: ${chooseIndent(children)}`;
+        case 'unmodified':
+          return `${indent1}${name}: ${format(value)}`;
         case 'modified':
           return [
-            `${indent2}- ${name}: ${oldValue}`,
-            `${indent2}- ${name}: ${newValue}`,
+            `${indent2}- ${name}: ${format(oldValue)}`,
+            `${indent2}+ ${name}: ${format(newValue)}`,
           ];
         default:
           throw new Error(`Unknown status: ${status}!`);
