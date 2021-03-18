f65ccb86dba792bc9a3d9ab543cf774524f66eec
andrew m <kaboom2012@mail.ru>
2021-03-18 05:36:36 +0300

    fix fullPath syntax

--- src/index.js
+++ src/index.js
@@ -27,13 +27,13 @@ const makeDiff = (obj1, obj2) => {
 
 export default (filepath1, filepath2, outputFormat) => {
   const currentDir = process.cwd();
-  const fullPath1 = path.resolve(currentDir, filepath1);
-  const fullPath2 = path.resolve(currentDir, filepath2);
+  const fullPath1 = path.resolve(currentDir, '__fixtures__', filepath1);
+  const fullPath2 = path.resolve(currentDir, '__fixtures__', filepath2);
   const data1 = fs.readFileSync(fullPath1, 'utf-8');
   const data2 = fs.readFileSync(fullPath2, 'utf-8');
-  const getFormat = (filePath) => path.extname(filePath).slice(1);
-  const obj1 = parse(data1, getFormat(fullPath1));
-  const obj2 = parse(data2, getFormat(fullPath2));
+  const getFileExtention = (filePath) => path.extname(filePath).slice(1);
+  const obj1 = parse(data1, getFileExtention(fullPath1));
+  const obj2 = parse(data2, getFileExtention(fullPath2));
   const diff = makeDiff(obj1, obj2);
 
   return format(diff, outputFormat);
