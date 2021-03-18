1b894a6c15fcdd4cf5d111bd2fabea39dd645098
andrew m <kaboom2012@mail.ru>
2021-03-18 04:23:05 +0300

    add main function signature

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,3 +2,13 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
+export default (diff, outputFormat) => {
+  switch (outputFormat) {
+    case 'plain':
+      return plain(diff);
+    case 'json':
+      return json(diff);
+    default:
+      return stylish(diff);
+  }
+};
