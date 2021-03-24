4fb933f5e5f063fa0e2f810d7ba78057089d64a4
andrew m <kaboom2012@mail.ru>
2021-03-24 03:25:11 +0300

    fix action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,5 +11,5 @@ const genDiff = program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'choose output format: stylish | plain | json', 'stylish')
   .action((filepath1, filepath2) => console.log(getDiff(filepath1, filepath2, program.format)))
-  .parse(process.action);
+  .parse(process.argv);
 export default genDiff;
