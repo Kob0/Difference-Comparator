006813fb7a9040dd4edab8d7c5733ae685d6befe
andrew m <kaboom2012@mail.ru>
2021-03-31 23:28:52 +0300

    replace main functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import _ from 'lodash';
-import fs from 'fs';
-import path from 'path';
-import parse from './parsers.js';
-import format from './formatters/index.js';
 
 const makeDiff = (obj1, obj2) => {
   const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
@@ -25,16 +21,4 @@ const makeDiff = (obj1, obj2) => {
   });
 };
 
-export default (filepath1, filepath2, outputFormat) => {
-  const currentDir = process.cwd();
-  const fullPath1 = path.resolve(currentDir, '__fixtures__', filepath1);
-  const fullPath2 = path.resolve(currentDir, '__fixtures__', filepath2);
-  const data1 = fs.readFileSync(fullPath1, 'utf-8');
-  const data2 = fs.readFileSync(fullPath2, 'utf-8');
-  const getFileExtention = (filePath) => path.extname(filePath).slice(1);
-  const obj1 = parse(data1, getFileExtention(fullPath1));
-  const obj2 = parse(data2, getFileExtention(fullPath2));
-  const diff = makeDiff(obj1, obj2);
-
-  return format(diff, outputFormat);
-};
+export default makeDiff;
