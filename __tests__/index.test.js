b131c4409c467c6878a80e65313388d5b3abacf2
andrew m <kaboom2012@mail.ru>
2021-04-01 02:21:59 +0300

    change full data

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import { test, expect, describe } from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
@@ -9,24 +9,30 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const cases = [
-  ['json', 'stylish', 'expectedStylish.txt'],
-  ['yml', 'stylish', 'expectedStylish.txt'],
-  ['json', 'plain', 'expectedPlain.txt'],
-  ['yml', 'plain', 'expectedPlain.txt'],
-  ['json', 'json', 'expectedJson.txt'],
-  ['yml', 'json', 'expectedJson.txt'],
-];
+const jsonFile1 = getFixturePath('file1.json');
+const jsonFile2 = getFixturePath('file2.json');
+const ymlFile1 = getFixturePath('file1.yml');
+const ymlFile2 = getFixturePath('file2.yml');
 
-describe('test genDiff, each case in order', () => {
-  test.each(cases)(
-    'files of type %p formatted in %p and expected to be as %p',
-    (type, format, expectedResult) => {
-      const file1 = getFixturePath(`file1.${type}`);
-      const file2 = getFixturePath(`file2.${type}`);
-      const diff = genDiff(file1, file2, format).trim();
-      const result = readFile(expectedResult).trim();
-      expect(diff).toEqual(result);
-    },
-  );
+const stylishDiff = readFile('expectedStylish.txt').trim();
+const jsonDiff = readFile('expectedJson.txt').trim();
+const plainDiff = readFile('expectedPlain.txt').trim();
+
+test('compare two files and shows a diff in stylish output format', () => {
+  expect(genDiff(jsonFile1, jsonFile2)).toBe(stylishDiff);
+  expect(genDiff(jsonFile1, jsonFile2, 'stylish')).toBe(stylishDiff);
+  expect(genDiff(ymlFile1, ymlFile2, 'stylish')).toBe(stylishDiff);
+  expect(genDiff(ymlFile1, jsonFile2, 'stylish')).toBe(stylishDiff);
+});
+
+test('compare two files and shows a diff in plain output format', () => {
+  expect(genDiff(jsonFile1, jsonFile2, 'plain')).toBe(plainDiff);
+  expect(genDiff(ymlFile1, ymlFile2, 'plain')).toBe(plainDiff);
+  expect(genDiff(ymlFile1, jsonFile2, 'plain')).toBe(plainDiff);
+});
+
+test('compare two files and shows a diff in json output format', () => {
+  expect(genDiff(jsonFile1, jsonFile2, 'json')).toBe(jsonDiff);
+  expect(genDiff(ymlFile1, ymlFile2, 'json')).toBe(jsonDiff);
+  expect(genDiff(ymlFile1, jsonFile2, 'json')).toBe(jsonDiff);
 });
