050273a4ebc81cdf8ad399dcf3c3086d8a0ac5cf
andrew m <kaboom2012@mail.ru>
2021-04-01 19:32:34 +0300

    fix default switch case

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,13 +2,15 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-export default (diff, outputFormat) => {
+export default (diff, outputFormat = 'stylish') => {
   switch (outputFormat) {
     case 'plain':
       return plain(diff);
     case 'json':
       return json(diff);
-    default:
+    case 'stylish':
       return stylish(diff);
+    default:
+      throw new Error(`Unknown output format ${outputFormat}!`);
   }
 };
